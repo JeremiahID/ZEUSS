b3b1b8c6c404ddcc7495cecd197bb807d8b929d1
JeremiahID <jeremiahefe.je@gmail.com>
2024-02-11 11:21:56 +0100

    test

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// mouse over scripting
+
+const Male = document.querySelector('#male');
+const Female = document.querySelector('#female');
+const Main = document.querySelector('main');
+
+
+Male.addEventListener('mouseover', () =>{
+    // Main.style.gridTemplateColumns = '65% 35%';
+
+})
