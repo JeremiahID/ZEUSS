0219c2af79f6b2dbfabbeb2c1a7c5c68eee489ef
JeremiahID <jeremiahefe.je@gmail.com>
2024-02-11 11:32:45 +0100

    layer setup

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,21 @@
 const Male = document.querySelector('#male');
 const Female = document.querySelector('#female');
 const Main = document.querySelector('main');
+const Male_layer = document.querySelector('#Male_layer')
+const Header = document.querySelector('h1');
 
 
 Male.addEventListener('mouseover', () =>{
-    // Main.style.gridTemplateColumns = '65% 35%';
+    if(Male_layer.style.display === "none"){
+        Male_layer.style.display = "block";
+        Main.style.gridTemplateColumns = '65% 35%';
+        Header.style.display = "none";
+    } else {
+        Male_layer.style.display = "none";
+        Main.style.gridTemplateColumns = '50% 50%';
+        Header.style.display = "block";
+    }
+   
+
 
 })
